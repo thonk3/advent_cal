b6ba7cf9f1439457ff6228f1163f93ab9c590313
debubz <bao.es.hoang2@gmail.com>
2020-12-07 12:51:22 +1100

    move the correct days into their folder

--- 2020/day03/something.js
+++ 2020/day03/something.js
@@ -1,138 +1,57 @@
-const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
 const fs = require('fs');
 
 var input = []
 const FILE = './input';
 fs.readFile(FILE, 'utf-8', (err, data) => {
+    // split each line
+    // console.log(object)
     input = data.split("\n");
-
-
-    let list = [];
-    let pass = {};
-    while (input.length != 0) {
-        let line = input.shift().split(" ");;
-        
-        // done with object
-        if(line[0] === "") {
-            list.push(pass);
-            // console.log(Object.keys(pass).length);
-            pass = {};
-        }
-        else {
-            line.forEach(phrase => {
-                let key = phrase.split(":")[0];
-                let val = phrase.split(":")[1];
-                // console.log(phrase, key, val);
-                pass = {
-                    ...pass,
-                    [key]: val,
-                }
-            })
-
-        }
-    }
-
-    console.log(list.length)
-    list = list.filter(item => Object.keys(item) !== 0);
-    console.log(list.length)
-    // console.log(list[list.length -1])
-
-
-    // // ======================================================================
-    // let valid = 0;
-    // list.forEach(item => {
-    //     let passField = Object.keys(item);
-    //     let vvv = [ 'ecl', 'iyr', 'hgt', 'byr', 'hcl', 'eyr', 'pid' ];
-
-    //     console.log(passField.length === 8);
-    //     if (passField.length === 8) {
-    //         valid++;
-    //         console.log("yes")
-    //     }
-    //     else if (passField.length === 7) {
-    //         if(passField.every(item => vvv.includes(item))) valid ++;
-    //     }
-
+    // input = input.map(i => {
+    //     return i.repeat(33);
     // })
+
+    // let valid = 0;
+    // let r = 0
+    // // ======================================================================
+    // for(let down = 0; down < input.length; down+=2) {
+    //     let line = [...input[down].toString()];
+
+    //     if(line[r%line.length] === '#') valid++;
+        
+    //     console.log(down, r%line.length)
+
+    //     r+=2;
+
+    // }
     // ======================================================================
-    let v2 = 0;
-    
-    const checkValid = pass => {
-        if ('byr' in pass) {
-            let isnum = /^\d+$/.test(pass.byr);
-            if(isnum) {
-                let n = parseInt(pass.byr);
-                if (n > 2002 || n < 1920)
-                    return false;
-            }
-            else return false;
-        }
-        if ('iyr' in pass) {
-            let isnum = /^\d+$/.test(pass.iyr);
-            if(isnum) {
-                let n = parseInt(pass.iyr);
-                if (n > 2020 || n < 2010)
-                    return false;
-            }
-            else return false;
-        }
-        if ('eyr' in pass) {
-            let isnum = /^\d+$/.test(pass.eyr);
-            if(isnum) {
-                let n = parseInt(pass.eyr);
-                if (n > 2030 || n < 2020) 
-                    return false;
-            }
-            else return false;
-        }
-        if('hgt' in pass) {
-            let nume = parseInt(pass.hgt.slice(0, pass.hgt.length-2));
-            let thing = pass.hgt.slice(pass.hgt.length -2, pass.hgt.length);
-            if (thing === "cm") {
-                if(nume > 193 || nume < 150) return false
-            }
-            else if (thing === "in") {
-                if (nume > 76 || nume < 59) return false; }
-            else return false;
+    // solution 2
+    let slope = [
+        { r: 1, d:1},
+        { r: 3, d:1},
+        { r: 5, d:1},
+        { r: 7, d:1},
+        { r: 1, d:2},
+    ]
+
+    let trees = []
+
+    slope.forEach(slope => {
+        let valid = 0;
+        let right = slope.r, d = slope.d;
+        let r = 0;
+
+        for(let i = 0; i < input.length; i+=d) {
+            let line = [...input[i].toString()];
+            if(line[r%line.length] === '#') valid++;
+            r+=right;
         }
 
-        if('hcl' in pass) {
-            let col = /^#([0-9a-f]+){6,6}/.test(pass.hcl);
-            if(!col) return false;
-        }
-
-        if('ecl' in pass) {
-            let eyeCol = ['amb', 'blu' ,'brn' ,'gry' ,'grn' ,'hzl' ,'oth']
-            if (!eyeCol.includes(pass.ecl)) return false
-        }
-
-        if('hcl' in pass) {
-            let col = /^#([0-9a-f]+){6}/.test(pass.hcl);
-            if(!col) return false;
-        }
-        if('pid' in pass) {
-            let col = /^\w{9}$/.test(pass.pid);
-            if(!col) return false;
-        }
-
-        return true;
-    }
-
-    list.forEach(item => {
-        let passField = Object.keys(item);
-        let vvv = [ 'ecl', 'iyr', 'hgt', 'byr', 'hcl', 'eyr', 'pid' ];
-
-        if (passField.length === 8) {
-            if (checkValid(item)) v2++;
-        }
-        else if (passField.length === 7) {
-            if(passField.every(item => vvv.includes(item))) {
-            if (checkValid(item)) v2++;
-            };
-        }
-
+        console.log(valid);
+        trees.push(valid)
     })
-    console.log(v2);
 
+    console.log(trees);
+    console.log(trees.reduce((a,b) => a*b));
 })
+
 
