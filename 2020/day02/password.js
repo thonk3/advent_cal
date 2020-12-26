ba34288e907223e205ea21e56450ffdc8a85986e
debubz <bao.es.hoang2@gmail.com>
2020-12-27 05:08:21 +1100

    streamlined day2

--- 2020/day02/password.js
+++ 2020/day02/password.js
@@ -13,71 +13,24 @@ const parsedInput = fileImport.parse(line => {
     }
 })
 
-    let valid = 0;
-
-    let maxVal = 0;
-    let minVal = 0;
+let valid1 = 0; valid2 = 0;
+let maxVal = 0;
+let minVal = 0;
+// ======================================================================
+// solution part 1
+parsedInput.forEach(pass => {
+    // part 1
+    let len = [...pass.check].filter(char => char === pass.char).length;
+    if (len <= pass.max && len >= pass.min) valid1++;
     // ======================================================================
-    // solution part 1
-    input.forEach(pass => {
-        let len = [...pass.check].filter(char => char === pass.char).length;
+    // part 2
+    let str = [...pass.check];
+    let char = pass.char; c1 = str[pass.min - 1], c2 = str[pass.max - 1];
+    let flag = false;
 
-        if(len <= pass.max && len >= pass.min) valid++;
-    })
-
-    console.log(valid);
-
-/*
-
-var input = []
-fs.readFile(FILE, 'utf-8', (err, data) => {
-    input = data.split("\n").map(line => {
-        let parts = line.split(" ");
-
-        return {
-            min: parseInt(parts[0].split("-")[0]),
-            max: parseInt(parts[0].split("-")[1]),
-            char: parts[1][0],
-            check: parts[2]
-        }
-    });
-
-    let valid = 0;
-
-    let maxVal = 0;
-    let minVal = 0;
-    // ======================================================================
-    // solution part 1
-    // input.forEach(pass => {
-    //     let len = [...pass.check].filter(char => char === pass.char).length;
-
-    //     if(len <= pass.max && len >= pass.min) valid++;
-    // })
-
-    // ======================================================================
-    // solution part 2
-    input.forEach(pass => {
-        let str = [...pass.check];
-        let char = pass.char; c1 = str[pass.min -1], c2 = str[pass.max -1];
-        let flag = false;
-
-        if(c1 === char && c1 !== c2) valid++;
-        if(c2 === char && c1 !== c2) valid++;
-    })
-
-
-    console.log("valid", valid);
-
-
+    if (c1 === char && c1 !== c2) valid2++;
+    if (c2 === char && c1 !== c2) valid2++;
 })
 
-// let input = [];
-// readline.on('line', (line) => {
-//     input.push(line.toString());
-//     // console.log(typeof line);
-// })
-
-// console.log(input[0]);
-console.log(input)
-
-*/
+console.log("Part 1:", valid1);
+console.log("Part 2:", valid2)
