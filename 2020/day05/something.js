4809fd4a9bf90da38d355809a9c4d0d6b90687ba
debubz <bao.es.hoang2@gmail.com>
2020-12-28 06:57:04 +1100

    day5 cleanup

--- 2020/day05/something.js
+++ 2020/day05/something.js
@@ -1,50 +1,39 @@
-
-const fs = require('fs');
-
-var input = []
 const FILE = './input';
-fs.readFile(FILE, 'utf-8', (err, data) => {
-    input = data.split("\n").map(item => {
-        let row = item.slice(0, 7).replace(/F/g, "0").replace(/B/g, "1");
-        let col = item.slice(7)
-            .replace(/R/g, "1")
-            .replace(/L/g, "0");
-        // R 1 L 0
-        // console.log(col);
-        return {
-            row, col
-        }
-    });
+const fi = require('../FileImport').default(FILE);
 
-    let max = 0;
-    const form = (row, col) => row * 8 + col;
+/* this be binary problem */
 
-    let zz = []
-
-
-    input.forEach(item => {
-        let a = parseInt(item.row, 2);
-        let b = parseInt(item.col, 2);
-
-        max = max > form(a, b) ? max : form(a, b)
-        //console.log(a)
-        zz.push(form(a, b))
-    })
-
-    // console.log(input[input.length-1])
-
-    // console.log(max)
-
-    input = input.map(i =>
-        form(parseInt(i.col, 2), parseInt(i.row, 2))
-    )
-
-    zz.sort((a, b) => a - b)
-
-    for (let i = 0; i < zz.length - 1; i++)
-        if (zz[i + 1] === zz[i] + 2) console.log(zz[i]);
-    // prints out id of -1;
-    // the real answer is +1
-
+let input = fi.parse(item => {
+    let row = item.slice(0, 7).replace(/F/g, "0").replace(/B/g, "1");
+    let col = item.slice(7).replace(/R/g, "1").replace(/L/g, "0");
+    // R 1 L 0
+    return { row, col }
 })
+
+// part 1
+let max = 0;
+const form = (row, col) => row * 8 + col;
+let zz = []
+
+input.forEach(item => {
+    let a = parseInt(item.row, 2);
+    let b = parseInt(item.col, 2);
+
+    max = max > form(a, b) ? max : form(a, b)
+    zz.push(form(a, b))
+})
+
+console.log("part 1:", max)
+
+
+// part 2
+input = input.map(i =>
+    form(parseInt(i.col, 2), parseInt(i.row, 2)))
+
+zz.sort((a, b) => a - b)
+
+for (let i = 0; i < zz.length - 1; i++)
+    if (zz[i + 1] === zz[i] + 2) console.log("part 2:", zz[i] + 1);
+
+
 
